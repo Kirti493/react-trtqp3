9e9731298eba092c90d529b9f232033bf733495b
Kirti493 <kirtisri493@gmail.com>
2021-04-19 16:45:45 +0530

    2 files modified

--- src/Flashcard.js
+++ src/Flashcard.js
@@ -5,11 +5,20 @@ export default function Flashcard({ flashcard }) {
 
   return (
     <div
+      className={`card ${flip ? "flip" : ""}`}
       onClick={() => {
         setFlip(!flip);
       }}
     >
-      {flip ? flashcard.answer : flashcard.question}
+      <div className="front">
+        {flashcard.question}
+        <div className="flashcard-options">
+          {flashcard.options.map(option => {
+            return <div className="flashcard-option">{option}</div>;
+          })}
+        </div>
+      </div>
+      <div className="back">{flashcard.answer}</div>
     </div>
   );
 }
