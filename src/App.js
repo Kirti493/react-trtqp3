cd5b81a7524b86c388d41a713f2d1379f1cebe00
Kirti493 <kirtisri493@gmail.com>
2021-04-20 13:28:24 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -7,20 +7,30 @@ export default function App() {
   const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS);
 
   useEffect(() => {
-    axios
-      .get('https://opentdb.com/api.php?amount=10');
-      .then(res => {
+    axios.get("https://opentdb.com/api.php?amount=10").then(res => {
+      setFlashcards(
         res.data.results.map((questionItem, index) => {
-          
+          const answer = decodeString(questionItem.correct_answer);
+          const options = [
+            ...questionItem.incorrect_answers.map(a => decodeString(a)),
+            answer
+          ];
           return {
             id: `${index}-${Date.now()}`,
-            questions: questionItem.question,
-            answer: questionItem.correct_answer,
-            options: 
-          }
+            question: decodeString(questionItem.question),
+            answer: answer,
+            options: options.sort(() => Math.random() - 0.5)
+          };
         })
-      })
+      );
+    });
   }, []);
+
+  function decodeString(str) {
+    const textArea = document.createElement("textarea");
+    textArea.innerHTML = str;
+    return textArea.value;
+  }
 
   return <FlashcardList flashcards={flashcards} />;
 }
